78f64ae8d13746fba1a3dc76192379be3745bd71
jkchao <jkchaom@gmail.com>
2018-12-01 14:54:52 +0800

    update options module

--- src/module/options/options.module.ts
+++ src/module/options/options.module.ts
@@ -3,11 +3,8 @@ import { OptionsService } from './options.service';
 import { OptionsResolver } from './options.resolvers';
 import { MongooseModule } from '@nestjs/mongoose';
 import { OptionsSchema } from './schema/options.shema';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Options } from './options.entity';
-
 @Module({
-  imports: [TypeOrmModule.forFeature([Options])],
+  imports: [MongooseModule.forFeature([{ name: 'Options', schema: OptionsSchema }])],
   providers: [OptionsService, OptionsResolver]
 })
 export class OptionsModule {}
